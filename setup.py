35584c8c4e3fc824037f488d168f6f8ce6b6ad83
Bunz <66bunz@gmail.com>
2023-09-15 10:39:37 +0200

    Fixed for new API changes
    Fixed outdated "page" parameter in product sales responses with the new "page_key" parameter.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pygumroad",
-    version="0.0.2",
+    version="0.0.3",
     author="Brennon Thomas",
     author_email="info@opsdisk.com",
     description="A Python API client for interacting with the Gumroad API.",
@@ -16,6 +16,7 @@ setuptools.setup(
     install_requires=[
         "requests>=2.24.0",
         "requests-toolbelt>=0.9.1",
+        "appengine-python-standard>=1.1.4"
     ],
     python_requires=">=3.6",
     license="GNU General Public License v3.0",
